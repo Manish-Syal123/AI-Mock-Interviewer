90ba97419318bb8724f26fb704e90d2ac2d25156
Manish_Syal <manishsyal77@gmail.com>
2024-07-13 17:59:36 +0530

    Added lottie animation

--- app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
@@ -5,6 +5,9 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import useSpeechToText from "react-hook-speech-to-text";
 import Webcam from "react-webcam";
+import voiceline from "./../../../../../../public/voicelines.json";
+import webcamlottie from "./../../../../../../public/webcamlottie.json";
+import Lottie from "lottie-react";
 
 const RecordAnswerSection = () => {
   const [userAnswer, setUserAnswer] = useState("");
@@ -30,13 +33,14 @@ const RecordAnswerSection = () => {
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="flex flex-col mt-20 justify-center items-center bg-black rounded-lg p-5">
-        <Image
+        {/* <Image
           src={"/webcam.png"}
           width={200}
           height={200}
           alt="webcam"
           className="absolute"
-        />
+        /> */}
+        <Lottie animationData={webcamlottie} loop={true} className="absolute" />
         <Webcam
           mirrored={true}
           style={{
@@ -53,8 +57,14 @@ const RecordAnswerSection = () => {
         className="my-10 border border-black"
       >
         {isRecording ? (
-          <h2 className="text-red-600 flex gap-2">
-            <AudioLines /> Stop Recording{" "}
+          <h2 className="text-red-600 flex gap-2 justify-center items-center text-center">
+            {/* <AudioLines /> Stop Recording{" "} */}
+            <Lottie
+              animationData={voiceline}
+              loop={true}
+              className="w-12 h-16"
+            />{" "}
+            <span>Stop Recording</span>
           </h2>
         ) : (
           "Record Answer"
