a73a7bffe0a20ae0f94a78a67b9b472cbfdf9ab4
Manish_Syal <manishsyal77@gmail.com>
2024-07-31 11:27:16 +0530

    Created Mobile Navbar/Header

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -2,15 +2,17 @@
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
+import { CgMenu, CgClose } from "react-icons/cg";
 
 const Header = () => {
+  const [menuIcon, setMenuIcon] = useState(false);
   const path = usePathname();
   const router = useRouter();
+
   return (
     <div
-      className={`flex p-4 items-center justify-between 
-      bg-secondary shadow-sm relative z-20 ${
+      className={`flex p-4 items-center bg-secondary justify-between shadow-sm relative z-20 ${
         path == "/dashboard/success" && "hidden"
       }`}
     >
@@ -20,37 +22,93 @@ const Header = () => {
         height={100}
         alt="logo"
         onClick={() => router.replace("/")}
+        className="cursor-pointer"
       />
-      <ul className="hidden md:flex gap-6">
+
+      <ul className={`hidden md:flex gap-6`}>
         <li
           onClick={() => router.replace("/dashboard")}
-          className={`font-medium hover:text-primary hover:font-extrabold 
-        transition-all cursor-pointer
-        ${path == "/dashboard" && "text-primary font-extrabold"}
-        `}
+          className={`font-medium hover:text-primary hover:font-extrabold transition-all cursor-pointer ${
+            path == "/dashboard" && "text-primary font-extrabold"
+          }`}
         >
           Dashboard
         </li>
         <li
           onClick={() => router.replace("/dashboard/favourites")}
-          className={`font-medium hover:text-primary hover:font-extrabold 
-        transition-all cursor-pointer
-        ${path == "/dashboard/favourites" && "text-primary font-extrabold"}
-        `}
+          className={`font-medium hover:text-primary hover:font-extrabold transition-all cursor-pointer ${
+            path == "/dashboard/favourites" && "text-primary font-extrabold"
+          }`}
         >
           Favourites
         </li>
         <li
           onClick={() => router.replace("/dashboard/upgrade")}
-          className={`font-medium hover:text-primary hover:font-extrabold 
-        transition-all cursor-pointer
-        ${path == "/dashboard/upgrade" && "text-primary font-extrabold"}
-        `}
+          className={`font-medium hover:text-primary hover:font-extrabold transition-all cursor-pointer ${
+            path == "/dashboard/upgrade" && "text-primary font-extrabold"
+          }`}
         >
           Upgrade
         </li>
       </ul>
-      <UserButton />
+
+      {/* Mobile Navbar section from here */}
+      <div className="flex items-center">
+        <UserButton />
+        <div className="md:hidden flex justify-end ml-4">
+          <button onClick={() => setMenuIcon(!menuIcon)}>
+            {menuIcon ? (
+              <CgClose className="text-2xl" />
+            ) : (
+              <CgMenu className="text-2xl" />
+            )}
+          </button>
+        </div>
+      </div>
+
+      <div
+        className={`absolute top-16 left-0 w-[30%] bg-secondary md:hidden transition-transform duration-300 rounded-lg ${
+          menuIcon ? "transform translate-x-0" : "transform -translate-x-full"
+        }`}
+      >
+        <ul className="flex flex-col items-center gap-6 py-4">
+          <li
+            onClick={() => {
+              router.replace("/dashboard");
+              setMenuIcon(false);
+            }}
+            className={`font-medium hover:text-primary hover:font-extrabold transition-all cursor-pointer ${
+              path == "/dashboard" && "text-primary font-extrabold"
+            }`}
+          >
+            Dashboard
+          </li>
+          <li
+            onClick={() => {
+              router.replace("/dashboard/favourites");
+              setMenuIcon(false);
+            }}
+            className={`font-medium hover:text-primary hover:font-extrabold transition-all cursor-pointer ${
+              path == "/dashboard/favourites" && "text-primary font-extrabold"
+            }`}
+          >
+            Favourites
+          </li>
+          <li
+            onClick={() => {
+              router.replace("/dashboard/upgrade");
+              setMenuIcon(false);
+            }}
+            className={`font-medium hover:text-primary hover:font-extrabold transition-all cursor-pointer ${
+              path == "/dashboard/upgrade" && "text-primary font-extrabold"
+            }`}
+          >
+            Upgrade
+          </li>
+        </ul>
+      </div>
+
+      {/* <UserButton /> */}
     </div>
   );
 };
