a6608104cb4a26f478fc5300cab5c06b49cdbdac
Manish_Syal <manishsyal77@gmail.com>
2024-08-15 14:32:29 +0530

    Added Demo Video source

--- app/_Serveractions/LandingPage.jsx
+++ app/_Serveractions/LandingPage.jsx
@@ -101,7 +101,7 @@ function LandingPage() {
                 </svg>
               </a>
               <a
-                href="https://www.linkedin.com/in/manish-syal-474222253/"
+                href="https://youtu.be/Zj_r9SoTbqc"
                 target="_blank"
                 className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
               >
